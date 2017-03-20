0bf20ba682d8c4f959063b107957fea4abfb85ae
Holmgren <kristofer.holmgren@sogeti.com>
2017-03-20 13:34:20 +0100

    Added typescript testing via Karma test-runner

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,6 +1,6 @@
 import * as Mocha from 'mocha';
 import { assert } from 'chai';
-import * as test from '../ts/app';
+import * as test from '../app/scripts/ts/app';
 
 describe('#Typescript function', function () {
   it('should always return 1', function () {
